65b51983079e394cd6adbd8441d4179196290202
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-06-17 21:26:20 +0200

    task3

--- Hw-2/Program.cs
+++ Hw-2/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers = { 5, 2, 8, 1, 4 };
+
+            Console.Write("1 - по возрастанию, 2 - по убыванию: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            bool ascending = (choice == 1);
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if ((ascending && numbers[i] > numbers[j]) ||
+                        (!ascending && numbers[i] < numbers[j]))
+                    {
+                        int temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Отсортированный массив:");
+
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
         }
     }
 }
